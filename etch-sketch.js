4292ae05f957a3217b8f76bb94f92fe841c6eec9
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2023-10-10 18:02:43 +0300

    Create Grids

---
+++ etch-sketch.js
@@ -0,0 +1,15 @@
+const container = document.querySelector('#container');
+let root = document.querySelector(':root');
+let rootStyles = getComputedStyle(root);
+let colNumber = rootStyles.getPropertyValue('--colNumber');
+
+
+function createDivs(gridNum){
+    for(i = 0; i < (gridNum*gridNum);i++){
+        let divs =document.createElement('div');
+        container.appendChild(divs);
+        divs.className = 'grids';
+    }
+    root.style.setProperty('--colNumber', gridNum);
+}
+createDivs(96);
