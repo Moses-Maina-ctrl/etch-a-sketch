919dcea0b92a0fffde70b0615eca6d93cde31c45
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2023-10-12 15:04:56 +0300

    Fix the slider

--- etch-sketch.js
+++ etch-sketch.js
@@ -4,8 +4,15 @@ let rootStyles = getComputedStyle(root);
 let colNumber = rootStyles.getPropertyValue('--colNumber');
 let slider = document.querySelector(".slider");
 let grids = slider.value;
-
 createDivs(grids);
+slider.onchange = function (){
+     grids = slider.value;
+     
+     deleteDivs();
+     createDivs(grids);
+
+}
+
 function createDivs(gridNum){
     for(i = 0; i < (gridNum*gridNum);i++){
         let divs =document.createElement('div');
@@ -13,5 +20,10 @@ function createDivs(gridNum){
         divs.className = 'grids';
     }
     root.style.setProperty('--colNumber', gridNum);
+    root.style.setProperty('--rowNumber', gridNum);
     
+}
+
+function deleteDivs(){
+    container.innerHTML = '';
 }
